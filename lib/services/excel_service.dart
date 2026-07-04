7289be8815465d5fddf301c338ca9d8137670699
moyassin1000 <54605864+moyassin1000@users.noreply.github.com>
2026-07-04 23:41:40 +0300

    Update excel_service.dart

--- lib/services/excel_service.dart
+++ lib/services/excel_service.dart
@@ -26,7 +26,7 @@ class PickedExcelFile {
 
 class ExcelService {
   Future<PickedExcelFile?> pickExcelFile() async {
-    final result = await FilePicker.platform.pickFiles(
+    final result = await FilePicker.pickFiles(
       type: FileType.custom,
       allowedExtensions: const ['xlsx'],
       allowMultiple: false,
